c13bf2c4cfbb9651c7d8fadd3b936d59d80a599a
iyo143 <limlimgo14@gmail.com>
2022-10-06 16:55:43 +0800

    add export

--- resources/views/livewire/pages/purchase-request.blade.php
+++ resources/views/livewire/pages/purchase-request.blade.php
@@ -3,8 +3,8 @@
         <i class="fa fa-fw fa-pencil-alt"></i>
     </a>
 
-    <button type="button" class="btn btn-sm btn-alt-danger" data-toggle="tooltip" title="Delete" wire:click="export" wire:loading.attr="disabled">
-        <i class="fa fa-fw fa-times"></i>
+    <button type="button" class="btn btn-sm btn-alt-info" data-toggle="tooltip" title="Export" wire:click="export" wire:loading.attr="disabled">
+        <i class="fa fa-fw fa-file-export"></i>
     </button>
     <button type="button" class="btn btn-sm btn-alt-danger" data-toggle="tooltip" title="Delete" wire:click="deleteConfirm" wire:loading.attr="disabled">
         <i class="fa fa-fw fa-times"></i>
